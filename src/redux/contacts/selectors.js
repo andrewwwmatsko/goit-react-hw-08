831a991a813a04a3d3fc3d4583c5382a51eebfdb
andrewwwmatsko <matskoandrew@gmail.com>
2024-07-31 17:36:41 +0300

    filter by number

--- src/redux/contacts/selectors.js
+++ src/redux/contacts/selectors.js
@@ -9,7 +9,10 @@ export const selectFilteredContacts = createSelector(
   [selectContacts, selectNameFilter],
   (contacts, filter) => {
     return contacts.filter((contact) => {
-      return contact.name.toLowerCase().includes(filter.toLowerCase());
+      return (
+        contact.name.toLowerCase().includes(filter.toLowerCase()) ||
+        contact.number.includes(filter)
+      );
     });
   }
 );
